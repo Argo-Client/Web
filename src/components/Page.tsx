97ed1bd2d436a2f2d88a0afcf195bfaf1b974d2c
guusvanmeerveld <guusvanmeerveld@live.nl>
2021-06-01 15:28:50 +0100

    Added home page

---
+++ src/components/Page.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+import { FC } from "react";
+
+const Page: FC<{
+  title: string;
+  description: string;
+  children: JSX.Element[] | JSX.Element;
+}> = ({ title, description, children }) => (
+  <>
+    <Head>
+      <title>Argo | {title}</title>
+      <meta property="og:title" content={title} />
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      <meta name="description" content={description} />
+      <meta property="og:description" content={description} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    </Head>
+    {children}
+  </>
+);
+export default Page;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,122 @@
 import { NextPage } from "next";
 
+import Page from "@components/Page";
+
+import { AiFillAndroid } from "react-icons/ai";
+import { FaAppStoreIos, FaGooglePlay, FaGithub, FaCoffee, FaTwitter } from "react-icons/fa";
+
+const LinkButton = ({ href, children }: { href: string; children: JSX.Element }) => (
+  <a href={href}>
+    <button>{children}</button>
+  </a>
+);
+
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1>Argo web</h1>
-    </div>
+    <Page title="Home" description="Ontdek Argo hier!">
+      <div>
+        <h1 id="home">Argo voor Magister 6</h1>
+        <p>
+          Argo is een moderne en simpele app voor Magister 6. Het doel is om de normale Magister app compleet te kunnen
+          vervangen door deze veel kleinere app.
+        </p>
+        <p>De app is beschikbaar voor Android, IOS and binnenkort nog meer platformen!</p>
+
+        <h2 id="download">Download</h2>
+        <LinkButton href="https://play.google.com/store/apps/details?id=nl.noxus.argo">
+          <FaGooglePlay size={25} />
+        </LinkButton>
+        <LinkButton href="">
+          <FaAppStoreIos size={25} />
+        </LinkButton>
+        <LinkButton href="">
+          <AiFillAndroid size={25} />
+        </LinkButton>
+
+        <h2 id="features">Features</h2>
+        <ul>
+          <li>Mooie, handige agenda waarin je precies kan zien wanneer je een tussenuur hebt.</li>
+          <li>
+            Een overzichtelijke thuispagina waarop je recente berichten, cijfers en absent-meldingen te zien krijgt.
+          </li>
+          <li>
+            Een handige manier om te berekenen welke cijfer je moet halen voor je volgende toets om nog voldoende te
+            staan.
+          </li>
+          <li>
+            Het pinnen van bronnen, studiewijzers of leermiddelen zodat je ze makkelijk terug kan vinden in je
+            thuispagina.
+          </li>
+          <li>Een uitgebreid assortiment aan instellingen zodat je Argo precies zo hebt zoals jij het wilt.</li>
+        </ul>
+
+        <table>
+          <caption>
+            <h2 id="version">Versie info</h2>
+          </caption>
+          <tbody>
+            <tr>
+              <th>Versie</th>
+              <td>0.3.0</td>
+            </tr>
+            <tr>
+              <th>Bestand</th>
+              <td>Placeholder</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <h2 id="building">Zelf bouwen</h2>
+
+        <h3>Vereisten</h3>
+        <ul>
+          <li>Flutter</li>
+          <li>Git</li>
+        </ul>
+
+        <h3>Stappen</h3>
+        <p>
+          Om Argo zelf te kunnen bouwen, moet je eerst Flutter hebben. Als je dit nog niet hebt, kan je dit downloaden
+          op <a href="https://flutter.dev/docs/get-started/install">de officiële website van Flutter</a>.
+        </p>
+        <p>Vervolgens kan je de source code downloaden door middel van het volgende commando:</p>
+        <pre>
+          <code>
+            git clone https://github.com/Argo-Client/app Argo <br />
+            cd Argo
+          </code>
+        </pre>
+
+        <p>Nu je de source code hebt, kan je de app bouwen met het volgende commando:</p>
+        <pre>
+          <code>flutter build</code>
+        </pre>
+
+        <h2 id="creators">Makers</h2>
+        <ul>
+          <li>
+            <a href="https://samtaen.nl">Sam Taen</a>
+          </li>
+          <li>
+            <a href="https://guusvanmeerveld.dev">Guus van Meerveld</a>
+          </li>
+          <li>
+            <a href="https://mboosterhuis.nl">Martijn Oosterhuis</a>
+          </li>
+        </ul>
+
+        <h2 id="links">Links</h2>
+        <LinkButton href="https://github.com/argo-client/app">
+          <FaGithub size={25} />
+        </LinkButton>
+        <LinkButton href="https://ko-fi.com/guusvanmeerveld">
+          <FaCoffee size={25} />
+        </LinkButton>
+        <LinkButton href="https://twitter.com/ClientArgo">
+          <FaTwitter size={25} />
+        </LinkButton>
+      </div>
+    </Page>
   );
 };
 
