ea057b4f78bbc5ad28dbc9eaaa20c8d92b8f2167
guusvanmeerveld <guusvanmeerveld@live.nl>
2021-06-01 14:08:45 +0100

    Added water.css and sass

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app";
 
 import "../styles/globals.scss";
+import "../styles/water.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Argo web</h1>
+    </div>
+  );
 };
 
 export default Home;
