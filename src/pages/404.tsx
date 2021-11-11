a721e0c004c83b89ebabf9f3d16715cfad3cb372
guusvanmeerveld <guusvanmeerveld@protonmail.com>
2021-11-11 10:59:19 +0100

    Front-end: Improved 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,30 @@
 import { NextPage } from "next";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Link from "next/link";
 
 import Page from "@components/Page";
 
-const NotFound: NextPage = () => (
-	<Page
-		title="Pagina niet gevonden"
-		description="De pagina is verwijderd of verplaatst"
-	>
-		<div>
-			<h1>Pagina niet gevonden</h1>
-			<h2>De pagina is verwijderd of verplaatst</h2>
-			<Link href="/">Ga terug</Link>
-		</div>
-	</Page>
-);
+const NotFound: NextPage = () => {
+	const { t } = useTranslation("404");
+
+	return (
+		<Page title={t("title")} description={t("description")}>
+			<div className="container">
+				<h1>{t("title")}</h1>
+				<h2>{t("description")}</h2>
+				<Link href="/">{t("goBack")}</Link>
+			</div>
+		</Page>
+	);
+};
+
+export const getStaticProps = async ({ locale }) => {
+	return {
+		props: {
+			...(await serverSideTranslations(locale, ["common", "404"])),
+		},
+	};
+};
 
 export default NotFound;
